c733239139c43e55717f99572f2a21dea874efaa
Chee <ck_white@outlook.com>
2024-01-17 22:17:08 -0500

    indextest.js

--- index.js
+++ index.js
@@ -2,4 +2,27 @@ const name = "Joe";
 const height = 74;
 const console.log(message); = `${name} is ${height} inches tall`;
 
-module.exports = {name, height,message};
+const {name, height,message} = require("../index.js");
+
+/*
+describe('what-is-a-test', () => {
+    describe('Name', () => {
+        it('returns "Susan", () => {
+            expect (name).toEqual('Susan')
+        })
+    })
+
+    describe('Height', () => {
+        it('is less than 40', () => {
+            expect (hight).toBeLessThan(40)
+         })
+    })
+
+    describe('Massage', () => {
+        it('gives the name and hight', () =>{
+            expect(message).toInclude(name)
+            expect(message).toInclude(hight)
+            })
+        })
+    })
+    
