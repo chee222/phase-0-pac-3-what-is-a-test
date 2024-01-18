c115cb95b64b018aef188945b7f30506bcdc9c70
Chee <ck_white@outlook.com>
2024-01-17 22:30:42 -0500

    mocha test

--- index.js
+++ index.js
@@ -2,27 +2,4 @@ const name = "Joe";
 const height = 74;
 const console.log(message); = `${name} is ${height} inches tall`;
 
-const {name, height,message} = require("../index.js");
-
-/*
-describe('what-is-a-test', () => {
-    describe('Name', () => {
-        it('returns "Susan", () => {
-            expect (name).toEqual('Susan')
-        })
-    })
-
-    describe('Height', () => {
-        it('is less than 40', () => {
-            expect (hight).toBeLessThan(40)
-         })
-    })
-
-    describe('Massage', () => {
-        it('gives the name and hight', () =>{
-            expect(message).toInclude(name)
-            expect(message).toInclude(hight)
-            })
-        })
-    })
-    
+module.exports = { name, height, message };
